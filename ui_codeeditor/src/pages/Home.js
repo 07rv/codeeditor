ec6cbf028e2f7bf751452816953591cf068f4ca1
Rohit Verma <28vrohit@gmail.com>
2024-09-14 22:04:33 +0530

    home page

--- ui_codeeditor/src/pages/Home.js
+++ ui_codeeditor/src/pages/Home.js
@@ -1,57 +1,37 @@
 const Home = () => {
   return (
-    <div class="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
-      <div class="relative py-3 sm:max-w-xl sm:mx-auto">
-        <div class="absolute inset-0 bg-gradient-to-r from-blue-300 to-blue-600 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
-        <div class="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
-          <div class="max-w-md mx-auto">
-            <div>
-              <h1 class="text-2xl font-semibold">Code With Me! </h1>
-            </div>
-            <div class="divide-y divide-gray-200">
-              <div class="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
-                <div class="relative">
-                  <input
-                    autocomplete="off"
-                    id="email"
-                    name="email"
-                    type="text"
-                    class="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600"
-                    placeholder="Email address"
-                  />
-                  <label
-                    for="email"
-                    class="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm"
-                  >
-                    Email Address
-                  </label>
-                </div>
-                <div class="relative">
-                  <input
-                    autocomplete="off"
-                    id="password"
-                    name="password"
-                    type="password"
-                    class="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600"
-                    placeholder="Password"
-                  />
-                  <label
-                    for="password"
-                    class="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm"
-                  >
-                    Password
-                  </label>
-                </div>
-                <div class="relative">
-                  <button class="bg-blue-500 text-white rounded-md px-2 py-1">
-                    Submit
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
+    <div className="min-h-screen bg-blue-400 flex justify-center items-center">
+      <div className="absolute w-60 h-60 rounded-xl bg-gray-300 -top-5 -left-16 z-0 transform rotate-45 hidden md:block"></div>
+      <div className="absolute w-48 h-48 rounded-xl bg-gray-300 -bottom-6 -right-10 transform rotate-12 hidden md:block"></div>
+      <div className="py-12 px-12 bg-white rounded-2xl shadow-xl z-20">
+        <div>
+          <h1 className="text-3xl font-bold text-center mb-4 cursor-pointer">
+            Code with me!
+          </h1>
+          <p className="w-80 text-center text-sm mb-8 font-semibold text-gray-700 tracking-wide cursor-pointer">
+            Do code with others effortlesly
+          </p>
+        </div>
+        <div className="space-y-4">
+          <input
+            type="text"
+            placeholder="Name"
+            className="block text-sm py-3 px-4 rounded-lg w-full border outline-gray-500"
+          />
+          <input
+            type="text"
+            placeholder="Room Id"
+            className="block text-sm py-3 px-4 rounded-lg w-full border outline-gray-500"
+          />
+        </div>
+        <div className="text-center mt-6">
+          <button className="w-full py-2 text-xl text-white bg-gray-400 rounded-lg hover:bg-gray-500 transition-all">
+            Join
+          </button>
         </div>
       </div>
+      <div className="w-40 h-40 absolute bg-gray-300 rounded-full top-0 right-12 hidden md:block"></div>
+      <div className="w-20 h-40 absolute bg-gray-300 rounded-full bottom-20 left-10 transform rotate-45 hidden md:block"></div>
     </div>
   );
 };
