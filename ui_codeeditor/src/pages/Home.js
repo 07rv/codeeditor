b589cab519470149db1b4a57fea7cbbfc06f350a
Rohit Verma <28vrohit@gmail.com>
2024-09-14 22:07:42 +0530

    .

--- ui_codeeditor/src/pages/Home.js
+++ ui_codeeditor/src/pages/Home.js
@@ -28,6 +28,19 @@ const Home = () => {
           <button className="w-full py-2 text-xl text-white bg-gray-400 rounded-lg hover:bg-gray-500 transition-all">
             Join
           </button>
+          <p
+            id="helper-text-explanation"
+            className="mt-2 text-sm text-gray-500 dark:text-gray-400"
+          >
+            Well never share your details. Read our{" "}
+            <a
+              href="#"
+              clasName="font-medium text-blue-600 hover:underline dark:text-blue-500"
+            >
+              Privacy Policy
+            </a>
+            .
+          </p>
         </div>
       </div>
       <div className="w-40 h-40 absolute bg-gray-300 rounded-full top-0 right-12 hidden md:block"></div>
