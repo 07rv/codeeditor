4219dc105aa31894b0a54278753e56795ba06aa5
Rohit Verma <28vrohit@gmail.com>
2024-09-15 11:40:23 +0530

    .

--- ui_codeeditor/src/store/slice/toasterSlice.js
+++ ui_codeeditor/src/store/slice/toasterSlice.js
@@ -3,36 +3,20 @@ import { createSlice } from "@reduxjs/toolkit";
 export const toasterSlice = createSlice({
   name: "toaster",
   initialState: {
-    value: {
-      message: "",
-      error: false,
-      warning: false,
-      info: false,
-      success: false,
-      open: false,
-    },
+    value: [
+      {
+        message: "",
+        error: false,
+        warning: false,
+        info: false,
+        success: false,
+        open: false,
+      },
+    ],
   },
   reducers: {
     setToaster: (state, action) => {
       state.value = action.payload;
-    },
-    setToasterMessage: (state, action) => {
-      state.value.message = action.payload;
-    },
-    setToasterError: (state, action) => {
-      state.value.error = action.payload;
-    },
-    setToasterWarning: (state, action) => {
-      state.value.warning = action.payload;
-    },
-    setToasterInfo: (state, action) => {
-      state.value.info = action.payload;
-    },
-    setToasterSuccess: (state, action) => {
-      state.value.success = action.payload;
-    },
-    setToasterOpen: (state, action) => {
-      state.value.open = action.payload;
     },
   },
 });
