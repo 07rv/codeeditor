0f07d5fa0f5678a86744678e6da24f359d47819d
Rohit Verma <28vrohit@gmail.com>
2024-09-16 22:09:01 +0530

    add icons

--- ui_codeeditor/src/pages/EditorPage.js
+++ ui_codeeditor/src/pages/EditorPage.js
@@ -1,50 +1,34 @@
+import {
+  UserIcon,
+  UserGroupIcon,
+  ArrowTopRightOnSquareIcon,
+  ClipboardDocumentIcon,
+} from "@heroicons/react/24/solid";
+
 const EditorPage = () => {
   return (
     <>
       <div class="relative bg-slate-900 w-screen h-screen pattern">
         <nav class="z-20 flex shrink-0 grow-0 justify-around gap-4 border-t p-2.5 shadow-lg backdrop-blur-lg border-slate-600/60 bg-slate-800/50 fixed top-2/4 -translate-y-2/4 left-6 min-h-[auto] min-w-[64px] flex-col rounded-lg border">
-          <p class="flex aspect-square min-h-[32px] w-16 flex-col items-center justify-center gap-1 rounded-md p-1.5 bg-sky-900 text-sky-50">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="currentColor"
-              class="w-6 h-6 shrink-0"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
-              />
-            </svg>
-
+          <button class="flex aspect-square min-h-[32px] w-16 flex-col items-center justify-center gap-1 rounded-md p-1.5 bg-sky-900 text-sky-50">
+            <UserIcon class="w-6 h-6 shrink-0" />
             <small class="text-center text-xs font-medium">Rohit</small>
-          </p>
-
+          </button>
+          <button class="flex h-16 w-16 flex-col items-center justify-center gap-1 text-gray-400">
+            <UserGroupIcon class="w-6 h-6 shrink-0" />
+            <small className="text-xs font-medium">Users</small>
+          </button>
           <hr class="border-gray-700/60" />
 
-          <a
-            href="/"
-            class="flex h-16 w-16 flex-col items-center justify-center gap-1 text-gray-400"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="currentColor"
-              class="w-6 h-6"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M8.25 21v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21m0 0h4.5V3.545M12.75 21h7.5V10.75M2.25 21h1.5m18 0h-18M2.25 9l4.5-1.636M18.75 3l-1.5.545m0 6.205l3 1m1.5.5l-1.5-.5M6.75 7.364V3h-3v18m3-13.636l10.5-3.819"
-              />
-            </svg>
+          <button class="flex h-16 w-16 flex-col items-center justify-center gap-1 text-gray-400">
+            <ClipboardDocumentIcon class="w-6 h-6 shrink-0" />
+            <small className="text-xs font-medium">Invite</small>
+          </button>
 
-            <small className="text-xs font-medium">Home</small>
-          </a>
+          <button class="flex h-16 w-16 flex-col items-center justify-center gap-1 text-gray-400">
+            <ArrowTopRightOnSquareIcon class="w-6 h-6 shrink-0" />
+            <small className="text-xs font-medium">Leave</small>
+          </button>
         </nav>
       </div>
     </>
