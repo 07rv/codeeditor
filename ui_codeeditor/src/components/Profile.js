d42ee686bd5914896ebb1cb2a084686bdc6baf0f
Rohit Verma <28vrohit@gmail.com>
2024-09-28 17:18:50 +0530

    remove warning

--- ui_codeeditor/src/components/Profile.js
+++ ui_codeeditor/src/components/Profile.js
@@ -7,34 +7,34 @@ import {
 const Profile = ({ user }) => {
   return (
     <>
-      <div class="flex w-full items-center justify-between space-x-6 p-6">
-        <div class="flex-1 truncate">
-          <div class="flex items-center space-x-3">
-            <h3 class="truncate text-sm font-medium text-gray-900">
+      <div className="flex w-full items-center justify-between space-x-6 p-6">
+        <div className="flex-1 truncate">
+          <div className="flex items-center space-x-3">
+            <h3 className="truncate text-sm font-medium text-gray-900">
               {user?.name}
             </h3>
           </div>
-          <p class="mt-1 truncate text-sm text-gray-500">Acount owner</p>
+          <p className="mt-1 truncate text-sm text-gray-500">Acount owner</p>
         </div>
-        <UserCircleIcon class="h-10 w-10 flex-shrink-0 rounded-full bg-gray-300" />
+        <UserCircleIcon className="h-10 w-10 flex-shrink-0 rounded-full bg-gray-300" />
       </div>
       <div>
-        <div class="-mt-px flex divide-x divide-gray-200">
-          <div class="flex w-0 flex-1">
+        <div className="-mt-px flex divide-x divide-gray-200">
+          <div className="flex w-0 flex-1">
             <a
               href="howpossible17@example.com"
-              class="relative -mr-px inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-bl-lg border border-transparent py-4 text-sm font-semibold text-gray-900"
+              className="relative -mr-px inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-bl-lg border border-transparent py-4 text-sm font-semibold text-gray-900"
             >
-              <EnvelopeIcon class="h-5 w-5 text-gray-400" />
+              <EnvelopeIcon className="h-5 w-5 text-gray-400" />
               Email
             </a>
           </div>
-          <div class="-ml-px flex w-0 flex-1">
+          <div className="-ml-px flex w-0 flex-1">
             <a
               href="tel:+1-202-555-0170"
-              class="relative inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-br-lg border border-transparent py-4 text-sm font-semibold text-gray-900"
+              className="relative inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-br-lg border border-transparent py-4 text-sm font-semibold text-gray-900"
             >
-              <ArrowTopRightOnSquareIcon class="w-5 h-5 shrink-0" />
+              <ArrowTopRightOnSquareIcon className="w-5 h-5 shrink-0" />
               Remove
             </a>
           </div>
--- ui_codeeditor/src/components/Users.js
+++ ui_codeeditor/src/components/Users.js
@@ -9,14 +9,14 @@ const Users = ({ handleMouseEnter, handleMouseLeave }) => {
       <div
         onMouseEnter={handleMouseEnter} // Show UserList on hover
         onMouseLeave={handleMouseLeave} // Hide UserList with a delay when not hovering
-        class="relative bg-slate-900 w-screen h-screen pattern"
+        className="relative bg-slate-900 w-screen h-screen pattern"
       >
-        <nav class="z-20 flex shrink-0 grow-0 justify-around gap-4 border-t p-2.5 shadow-lg backdrop-blur-lg border-slate-600/60 bg-slate-800/50 fixed top-2/4 -translate-y-2/4 left-32 min-h-[auto] min-w-[64px] flex-col rounded-lg border">
-          <ul class="grid grid-cols-1 gap-6 sm:grid-cols-1 lg:grid-cols-2">
+        <nav className="z-20 flex shrink-0 grow-0 justify-around gap-4 border-t p-2.5 shadow-lg backdrop-blur-lg border-slate-600/60 bg-slate-800/50 fixed top-2/4 -translate-y-2/4 left-32 min-h-[auto] min-w-[64px] flex-col rounded-lg border">
+          <ul className="grid grid-cols-1 gap-6 sm:grid-cols-1 lg:grid-cols-2">
             {users.map((user, key) => (
               <li
                 key={key}
-                class="col-span-1 divide-y divide-gray-200 rounded-lg bg-white shadow"
+                className="col-span-1 divide-y divide-gray-200 rounded-lg bg-white shadow"
               >
                 <Profile user={user} />
               </li>
